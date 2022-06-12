e11ade9a952ca47c7fd824acd5fe81597f4bc8b7
Ajmal987 <70273543+Ajmal987@users.noreply.github.com>
2022-06-12 18:55:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// question number solution
+
+var list = ['geek','geekster','geeky'];
+for(let i=0; i<=list.length; i++){
+   if(list[i]=='geekster'){
+   console.log(i)
+   }
+   }
+   list.push('ajmal');
+   list.push('aj');
+   console.log(list);
+   
+   list.pop();
+   console.log(list);
+   
+   //Question number 3 solution
+   
+     var arr=['a','b','c','d'];
+ let index1 = arr.indexOf('c');
+ console.log(index1);
+ 
+ //Question number 4 solution
+ 
+ var arr=['a','b','c','d'];
+ let index = arr.indexOf('e');
+ console.log(index);
+ 
+ //Question number 5
+ 
+  var abc = [50,60,90,80];
+	
+ 
+
+abc.sort()
+console.log(abc);
+let temp = abc[abc.length -1];
+console.log(temp)
+ 
